eef454827aeb7bfe3c52cba605867afa07df5dc2
Robert Gu <arcwandx86@gmail.com>
2025-06-30 00:23:00 -0500

    Simulate a basic Monty Hall problem

--- sim.py
+++ sim.py
@@ -4,12 +4,29 @@ import statistics
 import random
 import math
 
-trials = 50000
-n = 50
+trials = 100000
+n = 3
 
 def sim(i):
     print(f'{i}/{trials}', end='\r')
-    return random.randint(0, 15)
+
+    # The reward is behind a random door
+    d = random.randint(0, n-1)
+
+    # Choose a door at random
+    c = random.randint(0, n-1)
+
+    # Of the doors, open one at random that is not the chosen door and not the
+    # reward door
+    doors = list(set(range(n)) - set([d]) - set([c]))
+    o = doors[random.randint(0, len(doors)-1)]
+    doors = list(set(range(n)) - set([c]) - set([o]))
+
+    # Switch the door
+    if True:
+        c = doors[random.randint(0, len(doors)-1)]
+
+    return c == d
 
 data = [ sim(i) for i in range(trials) ]
 # print(data)
